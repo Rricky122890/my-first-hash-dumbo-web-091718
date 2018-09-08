4bd83568ed786c6cb320462f525e1880ede05afa
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-08 23:51:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-dogs = Hash.new 
-dogs["Snowy"] = "mine"
+hash = {"name" => "rico", "age" => 27}
   
 end
 
