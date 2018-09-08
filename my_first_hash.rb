a53c28322b727c29b47a314162bdc741663e2245
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-08 23:46:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,6 +22,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -32,13 +33,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest["gun power"] = 4
 
 
   # return the shipping_manifest hash below
-
+return shipping_manifest
 end
